65a1c97f620fa113392b7c9be699bfa3f9e2abbf
Phillip <redscientific@gmail.com>
2019-04-06 00:41:18 -0600

    Companion Class with basic functionality. More work to be done.
    UI loads. add/remove distinct RS_Device_Boxes works. Pushbuttons are ready to be assigned to functions once functions are created. Record and Freeze buttons are currently assigned to add and remove respectively for testing purposes with name of Device being taken from block note textbox.

---
+++ Companion_View.py
@@ -0,0 +1,19 @@
+# Author: Phillip Riskin
+# Date: Spring 2019
+# Project: Companion App
+# Company: Red Scientific
+
+import sys
+from RS_Companion_Window_Class import *
+
+
+def main():
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    app = QtWidgets.QApplication(sys.argv)
+    companion_app = QtWidgets.QMainWindow()
+    ui = Companion_Window(companion_app)
+    companion_app.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":main()
